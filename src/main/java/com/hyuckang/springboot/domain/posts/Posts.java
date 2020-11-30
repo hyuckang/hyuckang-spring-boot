35a5d7c96eb7f7fd04f433fd2ae07cd9c8ef7e21
hyuckang <hyuckang15@gmail.com>
2020-11-30 09:29:19 +0900

    add posts doamin, postsRepository

---
+++ src/main/java/com/hyuckang/springboot/domain/posts/Posts.java
@@ -0,0 +1,37 @@
+package com.hyuckang.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter // 클래스 내 모든 필드의 Getter 메소드 자동 추가(lombok)
+@NoArgsConstructor // 기본 생성자 자동 추가(lombok)
+@Entity // 테이블과 매핑될 클래스임을 나타낸다
+public class Posts {
+
+    @Id // 해당 테이블의 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스 생성(lombok)
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
